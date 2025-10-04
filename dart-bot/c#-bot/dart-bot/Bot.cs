a1c12d12dbb91ff270a7cdedbe0cf31082355542
fri3drich <58916170+fri3drich@users.noreply.github.com>
2025-10-04 10:06:52 +0200

    zwischenstand dartbot

---
+++ dart-bot/c#-bot/dart-bot/Bot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dartBot
+{
+    public class Bot
+    {
+
+        public string name { get; set; }
+        public List<List<string[]>> spiele { get; set; }  // spiele -> list von spielen -> jedes spiel list von wurfen -> jeder wurf int[3]
+
+        public int zeigerSpielzug { get; set; } = 0; // aktueller Spielzug in Spiel
+        public int zeigerWurf { get; set; } = 0; // Anzahl freie Würfe pro Spielerzug 3 = der erste
+
+        public int spielIdMerker { get; set; } = 1234; // Für reset des SpielzugZeigers
+
+        public int dartboardId { get; set; } = 1234; // DartboardId für Bot
+
+
+    }
+}
---
+++ dart-bot/c#-bot/dart-bot/GameUpdate.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace dartBot
+{
+    public class Statistik
+    {
+        public int anzWuerfeSpiel { get; set; }
+        public int anzWuerfeTurnier { get; set; }
+        public int summeSpiel { get; set; }
+        public int summeTurnier { get; set; }
+        public int anzMinWuerfeBisSpielende { get; set; }
+        public int maxPunkteProSpielzug { get; set; }
+        public double avgSpiel { get; set; }
+        public double avgTurnier { get; set; }
+        public int anzStrafpunkte { get; set; }
+        public int anzRandTreffer { get; set; }
+        public int anzWandTreffer { get; set; }
+        public int anzBull { get; set; }
+        public int anzBullseye { get; set; }
+        public int anzEinerFeld { get; set; }
+        public int anzDoubleFeld { get; set; }
+        public int anzTripleFeld { get; set; }
+        public int anzUeberworfen { get; set; }
+        public int highestFinish { get; set; }
+        public List<List<int>> getroffeneFelder { get; set; }
+    }
+
+    public class Spieler
+    {
+        public int id { get; set; }
+        public int dartboardId { get; set; }
+        public string name { get; set; }
+        public int punktestand { get; set; }
+        public int freieWuerfe { get; set; }
+        public Statistik statistik { get; set; }
+    }
+
+    public class GameMessage
+    {
+        public List<Spieler> spielerPlatzierung { get; set; }
+        public List<Spieler> spielerReihenfolge { get; set; }
+        public Spieler currentPlayer { get; set; }
+        public int kostenStrafpunkte { get; set; }
+        public int anzahlSpiele { get; set; }
+        public int spielId { get; set; }
+        public int punkteSpielzug { get; set; }
+        public int letzterWurf { get; set; }
+        public string gameState { get; set; }
+    }
+}
